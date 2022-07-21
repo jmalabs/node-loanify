fa13251c2cad5ef8f725967d657f1b97be09ea64
RichardMalabanan <johnrichard.malabanan@renewsoftware.co>
2022-07-21 22:51:32 +0800

    implement global context to manage some global states.

--- client/src/assets/wrappers/RegisterPage.js
+++ client/src/assets/wrappers/RegisterPage.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Wrapper = styled.section`
   display: grid;
@@ -9,7 +9,7 @@ const Wrapper = styled.section`
     margin-bottom: 1.38rem;
   }
   .form {
-    max-width: 400px;
+    max-width: 500px;
     border-top: 5px solid var(--primary-500);
   }
 
@@ -31,5 +31,5 @@ const Wrapper = styled.section`
     cursor: pointer;
     letter-spacing: var(--letterSpacing);
   }
-`
-export default Wrapper
+`;
+export default Wrapper;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Logo, FormRow, Alert } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { useAppContext } from "../context/appContext";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+  showAlert: false,
+};
 
 function Register() {
-  return <h1>Register</h1>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const { isLoading, showAlert, displayAlert } = useAppContext();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { email, name, password, isMember } = values;
+
+    if (!email || !name || !password || (!isMember && !name)) {
+      displayAlert();
+      return;
+    }
+  };
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  return (
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {showAlert && <Alert />}
+        {!values.isMember && (
+          <FormRow
+            name="name"
+            type="text"
+            handleChange={handleChange}
+            value={values.name}
+          />
+        )}
+
+        <FormRow
+          name="email"
+          type="email"
+          handleChange={handleChange}
+          value={values.email}
+        />
+        <FormRow
+          name="password"
+          type="password"
+          handleChange={handleChange}
+          value={values.password}
+        />
+        <button type="submit" className="btn btn-block" onClick={onSubmit}>
+          Submit
+        </button>
+        <p>
+          {values.isMember ? "Not a member yet" : "Already a member?"}
+          <button type="button" className="member-btn" onClick={toggleMember}>
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
 }
 
+// const Wrapper = styled.div`
+//   form {
+//     width: 400px;
+//     border-top: 5px solid var(--primary-500);
+//     padding: 2rem 2.5rem;
+//     margin: 3rem auto;
+//     background: var(--white);
+//     border-radius: var(--borderRadius);
+//     box-shadow: var(--shadow-2);
+//     max-width: 400px;
+
+//     .logo {
+//       margin: 0 auto;
+//       display: block;
+//     }
+
+//     h3 {
+//       text-align: center;
+//     }
+
+//     input {
+//       padding: 1rem;
+//     }
+//   }
+// `;
 export default Register;
